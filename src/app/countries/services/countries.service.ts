8fcbcdc269f011c83a416ddb15d78200b16cd42f
Juan-CT <ct.juan.88@gmail.com>
2024-09-17 19:52:41 +0200

    Fin sección 9; Mejora de la app en sección 10

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
-@Injectable({providedIn: 'root'})
-export class ServiceNameService {
+@Injectable({ providedIn: 'root' })
+export class CountriesService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
@@ -13,7 +13,19 @@ export class ServiceNameService {
 
   }
 
-  searchCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`);
+  searchCountryByAlphaCode(term: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${this.apiUrl}/alpha/${term}`)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0] : null),
+        catchError(error => of(null))
+      );
   }
+
+  searchAPI(term: string, petition: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.apiUrl}${petition}${term}`)
+      .pipe(
+        catchError(error => of([]))
+      );
+  }
+
 }
